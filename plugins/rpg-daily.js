5fc2789a5cc2ba03c49f884be1d08eff2a34e1c9
Misael <101156132+Misaelsharkbot@users.noreply.github.com>
2022-05-16 09:44:46 -0500

    Update rpg-daily.js

--- plugins/rpg-daily.js
+++ plugins/rpg-daily.js
@@ -1,5 +1,5 @@
-const free = 5000
-const prem = 20000
+const free = 500000
+const prem = 2000000
 
 let handler = async (m, { isPrems }) => {
   let time = global.db.data.users[m.sender].lastclaim + 86400000
