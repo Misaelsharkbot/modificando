a3a258b1ee820074e485cf0ee7eb7c0fddf63d93
Misael <101156132+Misaelsharkbot@users.noreply.github.com>
2022-05-16 10:22:41 -0500

    Update nsfw-lesbian.js

--- plugins/nsfw-lesbian.js
+++ plugins/nsfw-lesbian.js
@@ -2,8 +2,7 @@ import fetch from 'node-fetch'
 
 let handler = async (m, { conn, usedPrefix, command }) => {
 	if (!db.data.chats[m.chat].nsfw && m.isGroup) throw `❗El grupo no admite contenido nsfw \n\n Para habilitar escriba \n*${usedPrefix}enable* nsfw`
-  var {age} = db.data.users[m.sender]
-   if (age <17) throw conn.reply(m.chat, '❎ Eres menor de edad! vuelve cuando tengas más de 18 años', m) 
+  var {age} = db.data.users[m.sender] 
    
     let res = await fetch('https://meme-api.herokuapp.com/gimme/lesbians')
     if (!res.ok) throw await res.text()
