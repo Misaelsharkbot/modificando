7a1799d5f97a545e361920282b36222366e4a4b6
Misael <101156132+Misaelsharkbot@users.noreply.github.com>
2022-05-15 20:35:40 -0500

    Update handler.js

--- handler.js
+++ handler.js
@@ -249,7 +249,7 @@ export async function handler(chatUpdate) {
                 if (!('isBanned' in chat))
                     chat.isBanned = false
                 if (!('welcome' in chat))
-                    chat.welcome = false
+                    chat.welcome = true
                 if (!('detect' in chat))
                     chat.detect = false
                 if (!('sWelcome' in chat))
