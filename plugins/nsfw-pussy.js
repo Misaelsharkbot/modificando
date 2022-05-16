d4a416dcc32971159a152f8fb5b815bb27e53935
Misael <101156132+Misaelsharkbot@users.noreply.github.com>
2022-05-16 10:23:05 -0500

    Update nsfw-pussy.js

--- plugins/nsfw-pussy.js
+++ plugins/nsfw-pussy.js
@@ -3,7 +3,6 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn, usedPrefix, command }) => {
 	if (!db.data.chats[m.chat].nsfw && m.isGroup) throw `❗El grupo no admite contenido nsfw \n\n Para habilitar escriba \n*${usedPrefix}enable* nsfw`
   var {age} = db.data.users[m.sender]
-   if (age <17) throw conn.reply(m.chat, '❎ Eres menor de edad! vuelve cuando tengas más de 18 años', m) 
    
    let pw = ["https://meme-api.herokuapp.com/gimme/pussy", "https://meme-api.herokuapp.com/gimme/LegalTeens"] 
  let nk = pw[Math.floor(Math.random() * pw.length)]
