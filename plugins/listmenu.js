93c5be560ccdf0cfb6121fe7b064615bd614aab6
Misael <101156132+Misaelsharkbot@users.noreply.github.com>
2022-05-16 16:50:14 -0500

    Create listmenu.js

---
+++ plugins/listmenu.js
@@ -0,0 +1,76 @@
+import { xpRange } from '../lib/levelling.js'
+const { levelling } = '../lib/levelling.js'
+import moment from 'moment-timezone'
+let handler = async (m, { conn, usedPrefix, command, args, isOwner, isAdmin, isROwner }) => {
+wm = global.wm
+vs = global.vs
+let { exp, limit, level, role } = global.db.data.users[m.sender]
+let { min, xp, max } = xpRange(level, global.multiplier)
+const sections = [
+{
+title: `𝐋𝐈𝐒𝐓𝐀 𝐃𝐄 𝐎𝐏𝐂𝐈𝐎𝐍𝐄𝐒`,
+rows: [
+{title: "✨ | 𝐎𝐰𝐧𝐞𝐫", description: "𝙰𝙲𝚃𝙸𝚅𝙰 𝙾 𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰 𝙻𝙰 𝙱𝙸𝙴𝙽𝚅𝙴𝙽𝙸𝙳𝙰 𝙴𝙽 𝙴𝙻 𝙶𝚁𝚄𝙿𝙾", rowId: `.owner`},
+{title: "🌎 | 𝐃𝐨𝐧𝐚𝐫", description: "𝙴𝙻 𝙱𝙾𝚃 𝚂𝙴 𝚅𝚄𝙴𝙻𝚅𝙴 𝙳𝙴 𝚄𝚂𝙾 𝙿𝚄𝙱𝙻𝙸𝙲𝙾 𝚈/𝙾 𝙿𝚁𝙸𝚅𝙰𝙳𝙾", rowId: `.donar`},
+{title: "🥵 | 𝐁𝐨𝐭", description: "𝙰𝙲𝚃𝙸𝚅𝙰 𝙾 𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰 𝙻𝙾𝚂 𝙲𝙾𝙼𝙰𝙽𝙳𝙾𝚂 +𝟷𝟾", rowId: `.bot`},
+{title: "🔗 | 𝐌𝐞𝐧𝐮", description: "𝙰𝙲𝚃𝙸𝚅𝙰 𝙾 𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰 𝙴𝙻 𝙰𝙽𝚃𝙸 𝙴𝙽𝙻𝙰𝙲𝙴𝚂 𝙳𝙴 𝙶𝚁𝚄𝙿𝙾𝚂 𝙳𝙴 𝚆𝙷𝙰𝚃𝚂𝙰𝙿𝙿", rowId: `.menu`},   
+{title: "🔗 | 𝐌𝐞𝐧𝐮2", description: "𝙰𝙲𝚃𝙸𝚅𝙰 𝙾 𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰 𝙴𝙻 𝙰𝙽𝚃𝙸 𝙴𝙽𝙻𝙰𝙲𝙴𝚂 𝚀𝚄𝙴 𝙸𝙽𝙸𝙲𝙸𝙰𝙽 𝙴𝙽 𝙷𝚃𝚃𝙿𝚂", rowId: `.menu2`},    
+{title: "🔎 | 𝐈𝐧𝐟𝐨", description: "𝙰𝙲𝚃𝙸𝚅𝙰 𝙾 𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰 𝙻𝙰𝚂 𝙽𝙾𝚃𝙸𝙵𝙸𝙲𝙰𝙲𝙸𝙾𝙽𝙴𝚂 𝙳𝙴 𝙽𝚄𝙴𝚅𝙰 𝙼𝙾𝙳𝙸𝙵𝙸𝙲𝙰𝙲𝙸𝙾𝙽 𝙴𝙽 𝚄𝙽 𝙶𝚁𝚄𝙿𝙾", rowId: `.info`},      
+{title: "❗ | 𝐌𝐚𝐭𝐞𝐬", description: "𝙰𝙲𝚃𝙸𝚅𝙰 𝙾 𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰 𝙻𝙰𝚂 𝚁𝙴𝚂𝚃𝚁𝙸𝙲𝙲𝙸𝙾𝙽𝙴𝚂 𝙿𝙰𝚁𝙰 𝚂𝙰𝙲𝙰𝚁 𝙶𝙴𝙽𝚃𝙴 𝙳𝙴 𝙶𝚁𝚄𝙿𝙾𝚂", rowId: `.mates`},
+]}, ]
+let name = await conn.getName(m.sender)
+const listMessage = {
+title: `╭─────────────────────❀\n│${ucapan()}\n│💖•.¸💝¸.• *${name}* •.¸💝¸.•💖\n╰─────────────────────❀`,
+description: `╭━━━━━━〔 *${wm}* 〕━━━━━━⬣
+┃✪ *Tiempo Actual | Current Time*	    
+┃➺ ${time}   
+┃   ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+┃✪ *Activa durante | Active during* 
+┃➺ ${uptime}
+┃   ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+┃✪ *Versión de GataBot-MD*
+┃➺ ${vs}
+┃   ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+┃✪ *Usuario(s) | Users*
+┃➺ ${Object.keys(global.db.data.users).length} 
+┃   ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+┃✪ *Modo | Mode*
+┃➺ ${global.opts['self'] ? 'Self' : 'publik'}
+┃   ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+┃✪ *Chat(s) Prohibido(s) | Forbidden Chats*
+┃➺ ${Object.entries(global.db.data.chats).filter(chat => chat[1].isBanned).length} 
+┃   ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+┃✪ *Usuario(s) Prohibido(s) | Prohibited Users*
+┃➺ ${Object.entries(global.db.data.users).filter(user => user[1].banned).length}
+╰━━━━━━━━━━━━━━━━━━━━━⬣`,
+title: null,
+buttonText: "𝐒𝐄𝐋𝐄𝐂𝐂𝐈𝐎𝐍𝐄 𝐀𝐐𝐔𝐢",
+sections }}
+handler.help = ['en', 'dis'].map(v => v + 'able <option>')
+handler.tags = ['group', 'owner']
+handler.command = /^(esta)$/i
+export default handler
+
+function clockString(ms) {
+  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+  let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+  return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
+}
+function ucapan() {
+  const time = moment.tz('America/Los_Angeles').format('HH')  //America/Los_Angeles  Asia/Jakarta   America/Toronto
+  res = "👋 *BIENVENIDO(A)* 👋"
+  if (time >= 4) {
+    res = "🌇 *Buenos Días* ⛅"
+  }
+  if (time >= 11) {
+    res = "🏙️ *Buenas Tardes* 🌤️"
+  }
+  if (time >= 15) {
+    res = "🌆 *Buenas tardes* 🌥️"
+  }
+  if (time >= 17) {
+    res = "🌃 *Buenas noches* 💫"
+  }
+  return res
+}
