8c0d143514650340b3a446ffc7f57124f2bcd366
Misael <101156132+Misaelsharkbot@users.noreply.github.com>
2022-05-16 10:21:36 -0500

    Update nsfw-cosplay.js

--- plugins/nsfw-cosplay.js
+++ plugins/nsfw-cosplay.js
@@ -3,8 +3,7 @@ import fetch from 'node-fetch'
 import axios from 'axios'
 let handler = async (m, { conn, command, usedPrefix }) => {
 if (!db.data.chats[m.chat].nsfw && m.isGroup) throw `❗El grupo no admite contenido nsfw \n\n Para habilitar escriba \n*${usedPrefix}enable* nsfw`
-var {age} = db.data.users[m.sender]
-   if (age <17) throw conn.reply(m.chat, '❎ Eres menor de edad! vuelve cuando tengas más de 18 años', m) 
+var {age} = db.data.users[m.sender] 
 let cosp = (await axios.get(`https://raw.githubusercontent.com/FG98F/team-fg/main/img/pack.json`)).data 
 let url = cosp[Math.floor(Math.random() * cosp.length)]
 conn.sendButton(m.chat, `✅ Resultado 🤭`, `Vea más fotos aquí \nhttps://t.me/+8SKOTyja8rBlYTlh \n\n`, url, [['▷▷ Siguiente', `${usedPrefix + command}`]], m)
