144b33ab9f91961847ea8229a813107722efa9da
Misael <101156132+Misaelsharkbot@users.noreply.github.com>
2022-05-16 11:08:18 -0500

    Update gp-profile.js

--- plugins/gp-profile.js
+++ plugins/gp-profile.js
@@ -31,6 +31,7 @@ let str = `
 ▢ *🆙 Nivel* : ${level}
 ▢ *📇 Registrado :* ${registered ? 'Si': 'No'}
 ▢ *⭐ Premium* : ${prem ? 'Si' : 'No'}
+▢ *🚫 Baneado* : No
 └──────────────`
 conn.sendButton(m.chat, str, igfg, await(await fetch(pp)).buffer(), [['\n\nSoy guapo😎', '.s https://telegra.ph/file/51d0ced0e69012644134b.gif'], ['\n\nSoy feo😪', '.s https://telegra.ph/file/ec5f13d5b557bee4fc561.gif']], m)
 /*
