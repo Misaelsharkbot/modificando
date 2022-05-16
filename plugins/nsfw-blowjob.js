8a64b234138c82b44e96158960f93be3df090288
Misael <101156132+Misaelsharkbot@users.noreply.github.com>
2022-05-16 10:20:57 -0500

    Update nsfw-blowjob.js

--- plugins/nsfw-blowjob.js
+++ plugins/nsfw-blowjob.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn, usedPrefix, command }) => {
 	if (!db.data.chats[m.chat].nsfw && m.isGroup) throw `❗El grupo no admite contenido nsfw \n\n Para habilitar escriba \n*${usedPrefix}enable* nsfw`
   var {age} = db.data.users[m.sender]
-   if (age <17) throw conn.reply(m.chat, '❎ Eres menor de edad! vuelve cuando tengas más de 18 años', m) 
+  
     let res = await fetch('https://api.waifu.pics/nsfw/blowjob')
     if (!res.ok) throw await res.text()
     let json = await res.json()
