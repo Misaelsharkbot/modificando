ed5288cbfe63f04fbe9795d4e7e4e1200c8096e7
Misael <101156132+Misaelsharkbot@users.noreply.github.com>
2022-05-16 10:21:56 -0500

    Update nsfw-hentai.js

--- plugins/nsfw-hentai.js
+++ plugins/nsfw-hentai.js
@@ -2,8 +2,7 @@ import fetch from 'node-fetch'
 
 let handler = async (m, { conn, usedPrefix, command }) => {
 	if (!db.data.chats[m.chat].nsfw && m.isGroup) throw `❗El grupo no admite contenido nsfw \n\n Para habilitar escriba \n*${usedPrefix}enable* nsfw`
-  var {age} = db.data.users[m.sender]
-   if (age <17) throw conn.reply(m.chat, '❎ Eres menor de edad! vuelve cuando tengas más de 18 años', m) 
+  var {age} = db.data.users[m.sender] 
    
     let pw = ["https://api.waifu.pics/nsfw/blowjob", "https://api.waifu.pics/nsfw/neko", "https://api.waifu.pics/nsfw/waifu"] 
      let nk = pw[Math.floor(Math.random() * pw.length)]
